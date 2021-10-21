ca50c2bda407b525f7df53426416ae0e9e788c07
MasaHiroSaber <1171338853@qq.com>
2021-10-21 09:34:31 +0800

    2021.10.21 09:34

--- celiang.py
+++ celiang.py
@@ -9,7 +9,7 @@ while True:
     qianchiblackxia=input('前尺黑面下丝读数=')
     qianchiblackzhong=input('前尺黑面中丝读数=')
     qianchiredzhong=input('前尺红面中丝读数=')
-    K=input('K=')
+    step += 1
     houshijvli=(eval(houchiblackshang)-eval(houchiblackxia))*100
     qianshijvli=(eval(qianchiblackshang)-eval(qianchiblackxia))*100
     qianhoushijvcha=abs(houshijvli-qianshijvli)
@@ -23,14 +23,24 @@ while True:
         print('后视距离过大')
     if qianhoushijvcha>5:
         print('前，后视距差过大')
-    houchirbdushucha=abs(eval(houchibalckzhong)+eval(K)-eval(houchiredzhong))
-    print('后尺红黑面读数差=',round(houchirbdushucha,3),'m')
-    if houchirbdushucha>0.003:
-        print('后尺红黑面读数差过大')
-    qianchirbdushucha=abs(eval(qianchiblackzhong)+eval(K)-eval(qianchiredzhong))
-    print('前尺红黑面读数差=',round(qianchirbdushucha,3))
-    if qianchirbdushucha>0.003:
-        print('前尺红黑面读数差过大')
+    if step%2==1:
+        houchirbdushucha=eval(houchibalckzhong)+4.787-eval(houchiredzhong)
+        print('后尺红黑面读数差=',round(houchirbdushucha,3),'m')
+        if abs(houchirbdushucha)>0.003:
+            print('后尺红黑面读数差过大')
+        qianchirbdushucha=eval(qianchiblackzhong)+4.687-eval(qianchiredzhong)
+        print('前尺红黑面读数差=',round(qianchirbdushucha,3),'m')
+        if abs(qianchirbdushucha)>0.003:
+            print('前尺红黑面读数差过大')
+    elif step%2==0:
+        houchirbdushucha = eval(houchibalckzhong) + 4.687 - eval(houchiredzhong)
+        print('后尺红黑面读数差=', round(houchirbdushucha, 3), 'm')
+        if abs(houchirbdushucha) > 0.003:
+            print('后尺红黑面读数差过大')
+        qianchirbdushucha = eval(qianchiblackzhong) + 4.787 - eval(qianchiredzhong)
+        print('前尺红黑面读数差=', round(qianchirbdushucha, 3),'m')
+        if abs(qianchirbdushucha) > 0.003:
+            print('前尺红黑面读数差过大')
     liangchiheimiangaocha=eval(houchibalckzhong)-eval(qianchiblackzhong)
     liangchihongmiangaocha=eval(houchiredzhong)-eval(qianchiredzhong)
     print('两尺黑面高差=',round(liangchiheimiangaocha,3),'m')
@@ -38,14 +48,14 @@ while True:
     blackred=liangchiheimiangaocha-liangchihongmiangaocha
     if blackred<0:
         BRR=blackred+0.1
-        print('黑面高差与红面高差之差=', round(BRR, 3), 'm')
+        print('黑面高差与红面高差之差=', round(BRR, 4), 'm')
     elif blackred>0:
         BRR=blackred-0.1
-        print('黑面高差与红面高差之差=', round(BRR, 3), 'm')
+        print('黑面高差与红面高差之差=', round(BRR, 4), 'm')
     if blackred==0:
         BRR=0
         print('数据可能有误')
-    if BRR>0.005 :
+    if abs(BRR)>0.005 :
         print('黑面高差与红面高差之差过大')
     gaochazhongshus=liangchiheimiangaocha+liangchihongmiangaocha
     if gaochazhongshus<0:
@@ -55,18 +65,14 @@ while True:
         gaochazhongshu=(gaochazhongshus-0.1)/2
         print('高差中数=',round(gaochazhongshu,3),'m')
     F=F+qianhoushijvcha2
-    step+=1
+    print('视距累计差=', round(F, 3), 'm')
+    if F > 10:
+        print('视距累计差过大')
     print('当前测站编号为',step)
     time=input('继续测量请输入1/结束测量请输入2=')
     if time=='1':
-        print('视距累计差=',round(F,3),'m')
-        if F>10:
-            print('视距累计差过大')
         continue
     if time=='2':
-        print('视距累计差=',round(F,3),'m')
-        if F>10:
-            print('视距累计差过大')
         break
 
 
